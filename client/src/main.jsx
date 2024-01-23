8511e89ebd30afc1076b89ac25ddce1f3d7ba6db
CurtisCircus <cakrieg63@gmail.com>
2024-01-22 20:58:37 -0500

    Added more routing imports to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import Home from './pages/Home'
 import Error from './pages/Error'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import Employment from './pages/Employment'
+import Projects from './pages/Projects'
 
 const router = createBrowserRouter([
   {
